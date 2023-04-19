203f7543df1bf253ee3d45b5de55fc2f7a34be46
이충호 (주)소프트웍스/모바일 T월드 개발 <deed1515@softworks.co.kr>
2023-04-19 10:29:55 +0900

    [init] : 스프링 생성📰 java : 17, Spring 3.0.5

---
+++ src/main/java/com/ontrustserver/OnTrustServerApplication.java
@@ -0,0 +1,13 @@
+package com.ontrustserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnTrustServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnTrustServerApplication.class, args);
+    }
+
+}
