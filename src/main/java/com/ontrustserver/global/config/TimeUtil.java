56b8b323fc49d5e0f469fb67fd624663a160b6e2
Nontrust <deed1515@naver.com>
2023-05-16 18:10:16 +0900

    [fix] : utcTime 저장 버그 수정 :bug:

--- src/main/java/com/ontrustserver/global/config/TimeUtil.java
+++ src/main/java/com/ontrustserver/global/config/TimeUtil.java
@@ -2,13 +2,12 @@ package com.ontrustserver.global.config;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.time.*;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
 @Slf4j
 public class TimeUtil {
-    private final static String KTC = "Asia/Seoul";
     public static DateTimeFormatter formatter() {
         return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss zzzz");
     }
@@ -17,13 +16,6 @@ public class TimeUtil {
      * @return ZonedDateTime At UTC
      */
     public static ZonedDateTime nowUtcDate(){
-        return ZonedDateTime.from(Instant.now().atZone(ZoneOffset.UTC).toInstant());
-    }
-
-    public static ZonedDateTime convertUtcToKtc(Date utcDate){
-        ZoneId utcZoneId = ZoneId.of(KTC);
-        ZonedDateTime utcZonedDateTime = utcDate.toInstant().atZone(utcZoneId);
-
-        return utcZonedDateTime.withZoneSameInstant(utcZoneId);
+        return ZonedDateTime.now(ZoneOffset.UTC);
     }
 }
