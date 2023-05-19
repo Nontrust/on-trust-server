794ed0fb6039a1204d09e76d7945d381facf2ce8
Nontrust <deed1515@naver.com>
2023-05-19 17:16:03 +0900

    [chore] : MySQL8 이미지 생성을 위한 DockerFile 추가, 해당 위치 production docker에선 dockerignore처리 :card_file_box:

---
+++ mysql/sql/on_trust.sql
@@ -0,0 +1,24 @@
+create schema if not exists on_trust;
+use on_trust;
+
+create table if not exists post (
+    id bigint not null,
+    created_date datetime(6) not null,
+    updated_date datetime(6),
+    contents varchar(255),
+    title varchar(255),
+    primary key (id)
+) engine=InnoDB;
+
+create table if not exists post_seq (
+    next_val bigint
+) engine=InnoDB;
+
+# when post_seq is empty
+INSERT INTO post_seq
+SELECT 1
+FROM dual
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM post_seq
+);
