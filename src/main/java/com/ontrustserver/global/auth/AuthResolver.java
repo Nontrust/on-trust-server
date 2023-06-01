49532fa795bfb088cd5afcfb40682c1547b8abb1
Nontrust <deed1515@naver.com>
2023-06-01 17:01:34 +0900

    [prototype]: ArgumentResolver를 이용한 목 테스트 프로토타입 :safety_vest:

---
+++ src/main/java/com/ontrustserver/global/auth/AuthResolver.java
@@ -0,0 +1,30 @@
+package com.ontrustserver.global.auth;
+
+import com.ontrustserver.global.auth.domain.UserSession;
+import com.ontrustserver.global.exception.sup.Unauthorized;
+import org.springframework.core.MethodParameter;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import static org.springframework.util.StringUtils.hasText;
+
+public class AuthResolver implements HandlerMethodArgumentResolver{
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterType().equals(UserSession.class);
+    }
+
+    @Override
+    public Object resolveArgument(@Nullable MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        String authorization = webRequest.getHeader("Authorization");
+
+        if (!hasText(authorization)) {
+            throw new Unauthorized();
+        }
+
+        return new UserSession(1L, "MockUser");
+    }
+}
