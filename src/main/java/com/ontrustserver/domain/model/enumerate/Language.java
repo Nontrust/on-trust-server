9dfff4c768f47814a70a7f7fa7f067309ff3f775
Nontrust <deed1515@naver.com>
2023-06-15 12:23:10 +0900

    [feature]: Badword에 저장 될 Enum의 값으로 필터링 할 언어가 적용 될 UnicodeBlock을 설정 :sparkles:  - 저장 될 Record 필드 중 String 값이 어떤 언어로 되어있는 지 판단 될 값

--- src/main/java/com/ontrustserver/domain/model/enumerate/Language.java
+++ src/main/java/com/ontrustserver/domain/model/enumerate/Language.java
@@ -1,9 +1,26 @@
 package com.ontrustserver.domain.model.enumerate;
 
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+@Getter
 public enum Language {
     KOREAN,
     ENGLISH,
     JAPANESE,
     CHINESE
 
+    private final Character.UnicodeBlock unicodeBlock;
+
+    Language(Character.UnicodeBlock unicodeBlock) {
+        this.unicodeBlock = unicodeBlock;
+    }
+    public static TreeSet<String> getAllLanguageEnumNames() {
+        return Arrays.stream(Language.values())
+                .map(Enum::name)
+                .collect(Collectors.toCollection(TreeSet::new));
+    }
 }
